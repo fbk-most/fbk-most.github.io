7d198b56b967c0d99769b9a3873f38544b88b45c
Flake22 <flake@outlook.it>
2025-12-10 16:00:42 +0100

    refactor people with hive desing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 import { initCookies } from './cookies.js';
 import { initNews, initSlider } from './news.js';
 import { initTextAnimations, initHexagons } from './animations.js';
-import { initPeople } from './people.js';
+//import { initPeople } from './people.js';
 
 // Initialize after DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
@@ -10,5 +10,5 @@ document.addEventListener('DOMContentLoaded', () => {
   initNews();
   initSlider();
   initHexagons();
-  initPeople();
+  // initPeople();
 });
