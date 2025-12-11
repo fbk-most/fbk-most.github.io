57b88291c3b039727fce40d132d79ae7192804b4
Flake22 <flake@outlook.it>
2025-12-11 09:47:46 +0100

    removed deprecated files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 import { initCookies } from './cookies.js';
 import { initNews, initSlider } from './news.js';
 import { initTextAnimations, initHexagons } from './animations.js';
-//import { initPeople } from './people.js';
 
 // Initialize after DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
@@ -10,5 +9,4 @@ document.addEventListener('DOMContentLoaded', () => {
   initNews();
   initSlider();
   initHexagons();
-  // initPeople();
 });
